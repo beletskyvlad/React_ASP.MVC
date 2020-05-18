0644867dff8078531489efc0ed7a8d885684dd5c
beletskyvlad <beletskyvlad@gmail.com>
2020-05-18 14:12:41 +0300

    code clean up

--- MVC_App_With_React/Controllers/ReactController.cs
+++ MVC_App_With_React/Controllers/ReactController.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections.Generic;
-using System.Web.Mvc;
-using MVC_App_With_React.Models;
+﻿using System.Web.Mvc;
 
 namespace MVC_App_With_React.Controllers
 {
     public class ReactController : Controller
     {
-        private IList<CommentModel> _comments => CommentController._comments;
-
         // GET: react
         [Route("react")]
         public ActionResult Index()
         {
-            return View("React", _comments);
+            return View("React");
         }
     }
 }
